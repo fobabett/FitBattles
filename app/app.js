4545abd9e4c4a561af81015b616f3fea079f91c1
Te Vallee <astronomemusic@gmail.com>
2015-03-07 10:59:13 -1000

    passport setup in app.js

--- app/app.js
+++ app/app.js
@@ -1,5 +1,47 @@
 var express = require('express');
 var app = express();
+var passport = require('passport');
+var bodyParser = require('body-parser')
+LocalStrategy = require('passport-local').Strategy;
+
+
+// middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.engine('html', require('jade').__express);
+// app.set('view engine', 'html');
+// app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/')); 
+app.use(passport.initialize());
+app.use(passport.session());
+module.exports = app;
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({ username: username}, function(err, user) {
+      if(err) { 
+        return done(err);
+      }
+      if(!user) {
+        return done(null, flase, { message: 'Incorrect username.'});
+      }
+      if(!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password'});
+      }
+      return done(null, user);
+    });
+  }
+));
 
 app.get('/', function(req, res) {
   res.send('Hello World');
