162f2a51eb55fa0e3145ac5275781e17fd16f8b6
Te Vallee <astronomemusic@gmail.com>
2015-03-21 14:47:53 -1000

    activity/newsfeed schema

---
+++ app/models/activity.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose');
+var crypto = require('crypto');
+
+var activitySchema = mongoose.Schema({
+  user: String,
+  body: String,
+  weightLost: String,
+  date: {type: Date, default: Date.now}
+});
+
+var Activity = mongoose.model('activities', activitySchema);
+
+module.exports = Activity;
